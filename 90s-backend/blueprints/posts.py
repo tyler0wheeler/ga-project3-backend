be4b95136754da34f19cb1d0b4bc67d10c4eb755
Tyler Wheeler <tyler0wheeler@gmail.com>
2020-11-18 18:40:09 -0500

    update and delete added to post

--- 90s-backend/blueprints/posts.py
+++ 90s-backend/blueprints/posts.py
@@ -26,4 +26,30 @@ def create_posts():
     print(dir(post))
     print(model_to_dict(post), 'model to dict')
     post_dict = model_to_dict(post)
-    return jsonify(data=post_dict, status={"code": 201, "message": "Success"})
+    return jsonify(data=post_dict, status={"code": 200, "message": "Success"})
+
+@post.route('/<id>', methods=["GET"])
+def get_one_post(id):
+    # print(id, 'reserved word')
+    post = models.Post.get_by_id(id)
+    return jsonify(data=model_to_dict(post), status={"code": 200, "message": "Success"})
+
+@post.route('/<id>', methods=["PUT"])
+def update_post(id):
+    payload = request.get_json()
+    # print(payload)
+    query = models.Post.update(**payload).where(models.Post.id==id)
+    query.execute()
+    return jsonify(data=model_to_dict(models.Post.get_by_id(id)), status={"code": 200, "message": "Success"})
+
+@post.route('/<id>', methods=["DELETE"])
+def delete_post(id):
+    delete_query = models.Post.delete().where(models.Post.id==id)
+    num_of_rows_deleted = delete_query.execute()
+    print(num_of_rows_deleted)
+    # write logic -- if you have no rows deleted you will proabbly want some message telling you so
+    return jsonify(
+    data={},
+    message="Successfully deleted {} post with id {}".format(num_of_rows_deleted, id),
+    status={"code": 200}
+    )
